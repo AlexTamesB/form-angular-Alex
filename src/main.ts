af4386258df4f70cfea25da60cb81f886a97bfa3
AlexTamesB <jtamesb@vlacademy.com>
2025-05-28 22:17:08 -0600

    cambios

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+import { provideClientHydration } from '@angular/platform-browser';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
-import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideClientHydration(), ...appConfig.providers],
+});
